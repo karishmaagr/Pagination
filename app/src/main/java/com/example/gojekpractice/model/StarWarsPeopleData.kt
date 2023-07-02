a185a654256ec921c233ca0e7e74ca6866d02ec7
karishma <karishma@alignable.com>
2023-07-02 11:26:10 +0530

    data class correction

--- app/src/main/java/com/example/gojekpractice/model/StarWarsPeopleData.kt
+++ app/src/main/java/com/example/gojekpractice/model/StarWarsPeopleData.kt
@@ -3,20 +3,35 @@ package com.example.gojekpractice.model
 import com.google.gson.annotations.SerializedName
 
 data class StarWarsPeopleData(
-    @SerializedName("birth_year") val birth_year: String,
-    @SerializedName("created") val created: String,
-    @SerializedName("edited") val edited: String,
-    @SerializedName("eye_color") val eye_color: String,
-    @SerializedName("films") val films: List<String>,
-    @SerializedName("gender") val gender: String,
-    @SerializedName("hair_color") val hair_color: String,
-    @SerializedName("height") val height: String,
-    @SerializedName("homeworld") val homeworld: String,
-    @SerializedName("mass") val mass: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("skin_color") val skin_color: String,
-    @SerializedName("species") val species: List<String>,
-    @SerializedName("starships") val starships: List<String>,
-    @SerializedName("url") val url: String,
-    @SerializedName("vehicles") val vehicles: List<String>
+    @SerializedName("birth_year") val birth_year: String? = null,
+
+    @SerializedName("created") val created: String? = null,
+
+    @SerializedName("edited") val edited: String? = null,
+
+    @SerializedName("eye_color") val eye_color: String? = null,
+
+    @SerializedName("films") val films: List<String>? = null,
+
+    @SerializedName("gender") val gender: String? = null,
+
+    @SerializedName("hair_color") val hair_color: String? = null,
+
+    @SerializedName("height") val height: String? = null,
+
+    @SerializedName("homeworld") val homeworld: String? = null,
+
+    @SerializedName("mass") val mass: String? = null,
+
+    @SerializedName("name") val name: String? = null,
+
+    @SerializedName("skin_color") val skin_color: String? = null,
+
+    @SerializedName("species") val species: List<String>? = null,
+
+    @SerializedName("starships") val starships: List<String>? = null,
+
+    @SerializedName("url") val url: String? = null,
+
+    @SerializedName("vehicles") val vehicles: List<String>? = null
 )
